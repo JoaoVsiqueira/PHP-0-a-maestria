e957b97e68b7b2fbc2247eb2a9b15f5908adea7c
Joao victor de almeida siqueira <joao-vtsiqueira@hotmail.com>
2023-03-07 12:40:49 -0300

    modulo 2: caso sensitivo

---
+++ 2_conceitos_basicos/1_case_sentive/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Não é ccase sensitive
+echo "teste <br>";
+eChO "testando 2 <br>";
+ECHO "testando 3 <br>";
+
+//È case sensitive
+$nome = "João victor";
+$NOME = "João Victor";
+echo $nome;
+echo "<br>";
+echo $NOME;
+
+?>
+
