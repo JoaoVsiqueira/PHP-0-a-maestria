46b221eaa8f403592066b867d455d2c20a9b808d
Joao victor de almeida siqueira <joao-vtsiqueira@hotmail.com>
2023-03-07 16:54:16 -0300

    modulo 2 : comentários

---
+++ 2_conceitos_basicos/4-comentarios/inde.php
@@ -0,0 +1,15 @@
+<?php 
+
+ // Este é um comentário
+
+ echo "teste comentario";
+
+ # Apenas um comenário
+
+ /* 
+ Apenas 
+ comentários 
+ maiores 
+ */
+
+ ?>
---
+++ 2_conceitos_basicos/5_comentario_exercicio/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo " Exercicio <br> ";
+// comentário sobre o exercicio
+
+echo " Um jeito diferente de comentar no php <br> ";
+# comentario
+
+echo " Apenas comentários multi linhas <br> ";
+/* comentario para multi linha */
+
+?>
